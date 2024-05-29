6d997300781915ff433bcf28f0d81abe3b0fa0b8
RizaldyAli <113650859+RizaldyAli@users.noreply.github.com>
2024-05-29 01:15:36 +0000

    [STYLE] update logo

--- resources/views/layouts/landing/partials/footer.blade.php
+++ resources/views/layouts/landing/partials/footer.blade.php
@@ -2,12 +2,7 @@
     <div class="row justify-content-center">
         <div class="col-lg-4">
             <div class="text-center">
-                <a href="index-new.html">
-                    <img src="{{ asset('assets/landing/dist/images/logos/favicon.ico') }}" alt="" class="img-fluid pb-3">
-                </a>
-                <p class="mb-0 text-dark">All rights reserved by Modernize. Designed & Developed by <a
-                        class="text-dark text-hover-primary border-bottom border-primary"
-                        href="https://adminmart.com/">AdminMart.</a></p>
+                <h2 style="font-family: var(--bs-font-sans-serif) ;color: #50bb27; -webkit-text-stroke:1px #bef0a6;">Lingkungan Bersih</h2>
             </div>
         </div>
     </div>
